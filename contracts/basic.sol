6b58890d6ab19e40a08297b787b2fdcb4f1d3ccf
Moritz Hesche <mo.hesche@gmail.com>
2023-11-16 22:24:58 +0100

    more advanced proxy example patterns

--- contracts/basic.sol
+++ contracts/basic.sol
@@ -2,10 +2,14 @@
 
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/proxy/Proxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyImplementation {
+contract MyImplementation is UUPSUpgradeable, Ownable {
     uint256 public value;
+
+    constructor() Ownable(msg.sender) {}
 
     function setValue(uint256 _value) public {
         value = _value;
@@ -14,6 +18,8 @@ contract MyImplementation {
     function getValue() public view returns (uint256) {
         return value;
     }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
 
 interface IMyImplementation {
@@ -22,14 +28,16 @@ interface IMyImplementation {
     function getValue() external view returns (uint256);
 }
 
-contract MyProxy is Proxy, MyImplementation {
-    address internal logic;
+contract MyProxy is ERC1967Proxy {
+    constructor(address _implementation) ERC1967Proxy(_implementation, "") {}
 
-    constructor(address _logic) {
-        logic = _logic;
-    }
+    // address internal logic;
 
-    function _implementation() internal view override returns (address) {
-        return logic;
-    }
+    // constructor(address _logic) {
+    //     logic = _logic;
+    // }
+
+    // function _implementation() internal view override returns (address) {
+    //     return logic;
+    // }
 }
